9670ab0c2ab964577e9cad20db11f895317a1981
jhaboic <josiphaboic@gmail.com>
2022-01-11 14:43:10 +0100

    update

--- services/auth/src/middleware.rs
+++ services/auth/src/middleware.rs
@@ -1,3 +1,8 @@
+use std::{
+	pin::Pin,
+	task::{Context, Poll},
+};
+
 use actix_service::{Service, Transform};
 use actix_web::{
 	dev::{ServiceRequest, ServiceResponse},
@@ -39,4 +44,37 @@ where
 
 pub struct AuthenticationMiddleware<S> {
 	service: S,
+}
+
+impl<S, B> Service for AuthenticationMiddleware<S>
+where
+	S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+	S::Future: 'static,
+	B: 'static,
+{
+	type Error = Error;
+	type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+	type Request = ServiceRequest;
+	type Response = ServiceResponse<B>;
+
+	fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+		self.service
+			.poll_ready(cx)
+	}
+
+	fn call(&mut self, req: ServiceRequest) -> Self::Future {
+		println!("Hi from start. You requested: {}", req.path());
+
+		let fut = self
+			.service
+			.call(req);
+
+		Box::pin(async move {
+			let res = fut.await?;
+
+			println!("Hi from response");
+
+			Ok(res)
+		})
+	}
 }
